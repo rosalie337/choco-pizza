2c4c0c02a59e1a18fc2f14b5644fd72ae023e1ed
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-03-01 23:01:22 -0800

    added list.js

---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class List extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li> 
+
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
