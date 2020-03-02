3e2910c7ff9c660c379e6d47b3c9115f423747f3
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-03-01 23:16:01 -0800

    added a tab title index.html

--- src/header.js
+++ src/header.js
@@ -7,14 +7,14 @@ export default class Header {
                 <img src={this.props.logo} alt="logo" class="leftHeader"/>
                 <p class="leftHeader">{this.props.name}</p>
                 <h6 class="leftHeader">{this.props.description}</h6>
-                <img src={this.props.fb} alt="facebook logo" class="SocialNav"/>
-                <img src={this.props.twitter} alt="twitter logo" class="SocialNav"/>
-                <img src={this.props.google} alt="google plus logo" class="SocialNav"/>
-                <img src={this.props.instagram} alt="instagram logo" class="SocialNav"/>
-                <img src={this.props.flicker} alt="flicker logo" class="SocialNav"/>
-                <img src={this.props.pintrest} alt="pinterest logo" class="SocialNav"/>
-                <img src={this.props.rss} alt="rss logo" class="SocialNav"/>
-                <img src={this.props.email} alt="email logo" class="SocialNav"/>
+                <img src={this.props.fb} alt="facebook logo" class="socialNav"/>
+                <img src={this.props.twitter} alt="twitter logo" class="socialNav"/>
+                <img src={this.props.google} alt="google plus logo" class="socialNav"/>
+                <img src={this.props.instagram} alt="instagram logo" class="socialNav"/>
+                <img src={this.props.flicker} alt="flicker logo" class="socialNav"/>
+                <img src={this.props.pintrest} alt="pinterest logo" class="socialNav"/>
+                <img src={this.props.rss} alt="rss logo" class="socialNav"/>
+                <img src={this.props.email} alt="email logo" class="socialNav"/>
             </div>
     );
   }
